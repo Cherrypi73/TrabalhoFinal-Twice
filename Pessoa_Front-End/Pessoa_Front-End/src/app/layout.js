b02f52ab979125e7a518beb2d40d141d898ef403
Maria Luiza <passosbarbosademoraesmaria@gmail.com>
2023-06-12 14:24:03 -0400

    git meninamenina

--- Pessoa_Front-End/Pessoa_Front-End/src/app/layout.js
+++ Pessoa_Front-End/Pessoa_Front-End/src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Projeto aula',
+  title: 'Loja de Luxo',
   description: 'Simulando consumo de api com JsonServer',
 }
 
