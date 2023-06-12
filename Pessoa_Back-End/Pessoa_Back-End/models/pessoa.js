784f0822881050f1261fba57eb4e2b5fd7e94479
mariapassos23 <passosbarbosademoraesmaria@gmail.com>
2023-06-12 10:36:28 -0400

    git 3

--- Pessoa_Back-End/Pessoa_Back-End/models/pessoa.js
+++ Pessoa_Back-End/Pessoa_Back-End/models/pessoa.js
@@ -49,7 +49,7 @@ class Pessoa {
   static async delete(id) {
     try {
       const connect = await db.connect();
-      const sql = "DELETE FROM pessoas WHERE id=$1";
+      const sql = "DELETE FROM produto WHERE id=$1";
       return await connect.query(sql, [id]);
     } catch (error) {
       console.error('Erro em delete:', error);
@@ -58,4 +58,4 @@ class Pessoa {
   }
 }
 
-module.exports = Pessoa;
+module.exports = Produto;
