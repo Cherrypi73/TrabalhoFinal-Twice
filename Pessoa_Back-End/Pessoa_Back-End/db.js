8d527afc8315d64ff0124383a521e1c03c7e1926
mariapassos23 <90846606+mariapassos23@users.noreply.github.com>
2023-06-19 12:54:43 -0400

    comentarios

--- Pessoa_Back-End/Pessoa_Back-End/db.js
+++ Pessoa_Back-End/Pessoa_Back-End/db.js
@@ -1,14 +1,14 @@
-const { Pool } = require('pg');
+const { Pool } = require('pg'); //um cache de conexões de banco de dados mantido de forma que as conexões possam ser reutilizadas quando requisições futuras ao banco de dados forem requeridas
 
 let connect = async function () {
   try {
     if (global.connection) {
-      return Promise.resolve(global.connection);
+      return Promise.resolve(global.connection);// retorna resposta colocando na mesma api cliente e servidor
     }
 
     const pool = new Pool({
      connectionString:'postgres://dcfxefmr:InKsqLt_twa31O1IkLOBiC6gq98z-BL8@silly.db.elephantsql.com/dcfxefmr'
-    });
+    }); //conecta o banco de dados 
 
     global.connection = pool;
     return Promise.resolve(pool);
